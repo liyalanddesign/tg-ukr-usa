e3be0b8c95a48ca564777bb42301f9464c70c59a
user <user@users-MBP.lan>
2022-10-07 14:36:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
+const TG = window.Telegram.WebApp;
+
 function App() {
+  // FUNCTIONS
+  useEffect( ()=>{
+    TG.ready();
+  } )
+
+  const onClose = () => {
+    TG.close()
+    console.log("Window supposed to be closed")
+  }
+
+  // RENDER
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     works!
+     <button onClick={onClose}>Close Window!</button>
     </div>
   );
 }
